9f69c95092c77493c2f9d180e2fa2a9c334e92d8
wanghaikuan <wanghaikuan@nanshe-arch.com>
2025-06-18 20:11:13 +0800

    55

--- src/App.tsx
+++ src/App.tsx
@@ -69,16 +69,7 @@ function App() {
             // 翻转动画完成后添加卡片到选中区域
             setTimeout(() => {
               setSelectedCards(prev => [...prev, card]);
-              setMovingCard({
-                card: null,
-                sourceX: 0,
-                sourceY: 0,
-                targetX: 0,
-                targetY: 0,
-                isMoving: false,
-                isFlipping: false
-              });
-              //setSelectedCardAnimating(false);
+              setSelectedCardAnimating(false);
             }, 1000); // 翻转动画持续时间
           }, 500); // 移动到位置后的延迟
         }, 50); // 设置初始位置后的短暂延迟
